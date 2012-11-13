425b7557a143d41c56e3d6ed1a37157b127bb7a7
Vincent Ha <vincent.ha@gushcloud.com>
2012-11-13 09:37:54 +0800

    Updated profile page

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:vincent).profile_name
     assert_response :success
+    assert_template 'profiles/show'
   end
 
+  test "should render a 404 on profile page" do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
+
+  test "tht variables are assigned on successful profile viewing" do
+  	get :show, id: users(:vincent).profile_name
+  	assert assigns(:user)
+  	assert_not_empty assigns(:statuses)
+  end
+
+  test "only shows the correct user's statuses" do
+  	get :show, id: users(:vincent).profile_name
+  	assigns(:statuses).each do |status|
+  	  assert_equal users(:vincent), status.user
+  	end
+  end
 end
--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -17,5 +17,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-
+  test "that a profile page works" do
+    get '/vincentha'
+    assert_response :success
+  end
 end
